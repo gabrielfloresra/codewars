637b1afc22537963c63be5d7d71e53cc11c9a188
gabriel flores <gabriel.floresra@gmail.com>
2020-03-09 19:15:08 -0600

    problem: MovesInSquaredStrings(III) initialized...

---
+++ MovesInSquaredStrings(III)/main.go
@@ -0,0 +1,25 @@
+package main
+
+func rot90Clock(s string) string {
+	var result string
+	return result
+}
+func diag1Sym(s string) string {
+	var result string
+	return result
+}
+func selfieAndDiag1(s string) string {
+	var result string
+	return result
+}
+
+type fParam func(string) string
+
+func oper(f fParam, x string) string {
+	var result string
+	return result
+}
+
+func main() {
+
+}
